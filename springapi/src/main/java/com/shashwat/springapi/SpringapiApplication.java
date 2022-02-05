96d4bbfb96d3a27e24e5c156fb1fc9fa6cee1e5a
Shashwat Pathak <shashwatpathak98@gmail.com>
2022-02-06 00:20:34 +0530

    feat: Added REST Controller

--- springapi/src/main/java/com/shashwat/springapi/SpringapiApplication.java
+++ springapi/src/main/java/com/shashwat/springapi/SpringapiApplication.java
@@ -2,12 +2,19 @@ package com.shashwat.springapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringapiApplication.class, args);
 	}
 
+	@GetMapping
+	public String helloWorld(){
+		return "Hello World";
+	}
 }
